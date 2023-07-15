8ff185e0cb9f831c4009ebcac9702b755edc3f6e
Evaloper <gbajeeva@yahoo.com>
2023-07-16 00:53:27 +0100

    Header session

---
+++ src/components/atoms/Logo/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+export const Logo = () => {
+  return (
+    <div className="logo flex align-centered">
+      <div className="logo-img">
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+      </div>
+      <div className="logo-para">
+        <p>Payhelper</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/organisms/Header/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Logo } from '../../atoms/Logo'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <div className='top-header'>
+            <div className='logo'>
+                <Logo />
+                <div>
+                    
+                </div>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
