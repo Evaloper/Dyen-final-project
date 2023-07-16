6ade7e94cd0c0e662e73ebbc4a56488cadcea672
Evaloper <gbajeeva@yahoo.com>
2023-07-16 01:00:51 +0100

    Header session

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Logo } from '../../atoms/Logo'
+import { Logo, Icon } from '../../atoms'
+import "./index.css"
 
 export const Header = () => {
   return (
     <div className='header'>
-        <div className='top-header'>
+        <div className='top-header '>
             <div className='logo'>
                 <Logo />
                 <div>
-                    
+                  <Icon name="search"/>
                 </div>
+                <input type='text' placeholder='Search' />
             </div>
             <div>
                 <div></div>
